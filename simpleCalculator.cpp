42d1b6eef8b0fb2de28a8898eb602b6ed0c3fe4d
sibo hao <nielhaochina@gmail.com>
2020-11-04 22:18:16 +0100

    1

---
+++ simpleCalculator.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <math.h>
+
+double sum(double a, double b){
+    double res = a + b;
+    return res;
+}
+
+double subtract(double a, double b){
+    double res = a - b;
+    return res;
+}
+
+double mean(double a, double b){
+    double res = (a+b)/2;
+    return res;
+}
+double multiply(double a, double b){
+    double res = a * b;
+    return res;
+}
+double divide(int a, int b){
+    double res = (double)a/b;
+    return res;
+}
+double multiply(int a, int b){
+    double res = a * b;
+    return res;
+}
+double square(double a){
+    return a*a;
+}
+int main(){
+
+    double a = 3.35;
+    double b = 24;
+
+    int c = 49;
+    int d = 3;
+
+    std::cout << sum(a,b) << std::endl;
+    std::cout << subtract(a,b) <<std::endl;
+    std::cout << mean(a,b) <<std::endl;
+    std::cout << multiply(a,b) <<std::endl;
+    std::cout << divide(a,b) <<std::endl;
+    std::cout << multiply(c,d) <<std::endl;
+    std::cout << divide(c,d) <<std::endl;
+    std::cout << square(a) <<std::endl;
+
+    return 0;
+}
